201edf927c11736d0b671c5f8c7d84cb7d04490e
Shae Scotten <shaescotten@gmail.com>
2019-07-31 15:24:02 -0400

    add create-env

---
+++ create-env.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+fs.writeFileSync(
+  "./.env",
+  `
+BASE_URL=${process.env.BASE_URL}\n
+PROJECTS_URL=${process.env.PROJECTS_URL}\n
+SOCIAL_URL=${process.env.SOCIAL_URL}\n
+
+HOME_URL=${process.env.HOME_URL}\n
+ABOUT_URL=${process.env.ABOUT_URL}\n
+PROJECT_PAGE_URL=${process.env.PROJECT_PAGE_URL}\n
+DEMO_URL=${process.env.DEMO_URL}
+`
+);
